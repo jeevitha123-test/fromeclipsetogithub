7222385e2a714afd1914462354141f34588393da
jeevitha123-test <jeevithareddy.reddy9@gmail.com>
2024-09-11 17:59:06 +0530

    automation

--- ecliproject/fbautomationgit/src/test/java/fbeclipsetogithub/fbautomationgit/testscript/SampleTest.java
+++ ecliproject/fbautomationgit/src/test/java/fbeclipsetogithub/fbautomationgit/testscript/SampleTest.java
@@ -5,6 +5,7 @@ public class SampleTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("tsis for sample test");
+		System.out.println("purpose for testing");
 
 	}
 
